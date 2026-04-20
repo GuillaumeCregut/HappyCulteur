5c81024b82220445c569e55aae5e7b9f299edfad
Guillaume Crégut <gcregut@free.fr>
2026-04-20 18:44:04 +0200

    [Feat]: Params: create homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,9 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Apiculteur;
 use App\Service\UserConnected;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 #[Route('/', name: 'app_')]
@@ -58,5 +59,14 @@ final class HomeController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path:':/params', name: 'params')]
+    #[IsGranted('ROLE_USER')]
+    public function parameters(): Response
+    {
+         return $this->render('params/index.html.twig', [
+            
+        ]);
     }
 }
