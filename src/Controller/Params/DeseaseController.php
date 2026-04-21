79a9c38b309fb36ea12606a1916385a396b94c95
Guillaume Crégut <gcregut@free.fr>
2026-04-21 11:44:16 +0200

    [Feat]: Desease admin page: delete desease

--- src/Controller/Params/DeseaseController.php
+++ src/Controller/Params/DeseaseController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Params;
 
+use App\Entity\Desease;
+use App\Repository\DeseaseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -31,5 +35,17 @@ final class DeseaseController extends AbstractController
             'form' => $form
         ]);
     }
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/admin/desease/{id}', name: 'admin_desease_delete', methods: ['POST'])]
+    public function delete(
+        Request $request,
+        Desease $desease,
+        EntityManagerInterface $em,
+    ): Response {
+        if ($this->isCsrfTokenValid('delete' . $desease->getId(), $request->getPayload()->getString('_token'))) {
+            $em->remove($desease);
+            $em->flush();
+        }
+        return $this->redirectToRoute('app_admin_desease_index');
     }
 }
