d50de13f9f1e3bf82a1a449d9cc2551ab64cd7f5
Guillaume Crégut <gcregut@free.fr>
2026-04-21 17:04:48 +0200

    [Feat]: Admin Honey: create page

--- src/Controller/Params/HoneyController.php
+++ src/Controller/Params/HoneyController.php
@@ -39,8 +39,10 @@ final class HoneyController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             /**@var UploadedFile $hivePicture */
             $picture = $form->get('picture')->getData();
-            $picturePath = $this->handleFile($newHoney->getName(), $uploadDirectory, false, $picture);
-            $newHoney->setPicture($picturePath);
+            if (null !== $picture) {
+                $picturePath = $this->handleFile($newHoney->getName(), $uploadDirectory, false, $picture);
+                $newHoney->setPicture($picturePath);
+            }
             $em->persist($newHoney);
             $em->flush();
             return $this->redirectToRoute('app_params_honey_index');
@@ -51,10 +53,93 @@ final class HoneyController extends AbstractController
         ]);
     }
 
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route(path: 'admin/honey', name: 'admin_honey_index')]
+    public function adminHome(HoneyRepository $repo): Response
+    {
+        $honeys = $repo->findBy([], ['name' => 'ASC']);
+        return $this->render('admin/honey/index.html.twig', [
+            'honeys' => $honeys,
+        ]);
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route(path: 'admin/honey/new', name: 'admin_honey_new', methods: ['GET', 'POST'])]
+    public function add(
+        Request $request,
+        EntityManagerInterface $em,
+        #[Autowire('%kernel.project_dir%/public/uploads/')] string $uploadDirectory
+    ): Response {
+        $honey = new Honey();
+        $form = $this->createForm(HoneyType::class, $honey);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**@var UploadedFile $hivePicture */
+            $picture = $form->get('picture')->getData();
+            if (null !== $picture) {
+                $picturePath = $this->handleFile($honey->getName(), $uploadDirectory, false, $picture);
+                $honey->setPicture($picturePath);
+            }
+            $em->persist($honey);
+            $em->flush();
+            return $this->redirectToRoute('app_admin_honey_index');
+        }
+        return $this->render('admin/honey/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route(path: 'admin/honey/{id}/edit', name: 'admin_honey_update', methods: ['GET', 'POST'])]
+    public function update(
+        Honey $honey,
+        Request $request,
+        EntityManagerInterface $em,
+        #[Autowire('%kernel.project_dir%/public/uploads/')] string $uploadDirectory
+    ): Response {
+        $form = $this->createForm(HoneyType::class, $honey);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**@var UploadedFile $hivePicture */
+            $picture = $form->get('picture')->getData();
+            if (null !== $picture) {
+                $picturePath = $this->handleFile($honey->getName(), $uploadDirectory, false, $picture);
+                $honey->setPicture($picturePath);
+            }
+            $em->persist($honey);
+            $em->flush();
+            return $this->redirectToRoute('app_admin_honey_index');
+        }
+
+        return $this->render('admin/honey/edit.html.twig', [
+            'form' => $form,
+            'honey' => $honey
+        ]);
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route(path: 'admin/honey/{id}', name: 'admin_honey_delete', methods: ['POST'])]
+    public function delete(
+        Request $request,
+        EntityManagerInterface $em,
+        #[Autowire('%kernel.project_dir%/public/uploads/')] string $uploadDirectory,
+        Honey $honey
+    ): Response {
+        if ($this->isCsrfTokenValid('delete' . $honey->getId(), $request->getPayload()->getString('_token'))) {
+            $path = $honey->getPicture();
+            if(null !== $path) {
+                $this->handleFile($path, $uploadDirectory, true);
+            }
+            $em->remove($honey);
+            $em->flush();
+        }
+        return $this->redirectToRoute('app_admin_honey_index');
+    }
+
     private function handleFile(string $filename, string $rootPath,  bool $remove, ?UploadedFile $file = null): ?string
     {
         if ($remove) {
-            $picturePath = $rootPath . 'admin' . DIRECTORY_SEPARATOR .'honeys' . DIRECTORY_SEPARATOR . $filename;
+            $picturePath = $rootPath . $filename;
             unlink($picturePath);
             return null;
         }
@@ -64,7 +149,7 @@ final class HoneyController extends AbstractController
         $resultFilename = $this->uploader->storeFile($file, 'admin', 'honeys', $filename);
         try {
             $result = PictureFormator::format($rootPath . $resultFilename, PictureFormator::TO_PNG, 20, 20);
-            if(!$result) {
+            if (!$result) {
                 throw new Exception("Conversion file failed");
             }
         } catch (PictureException $e) {
