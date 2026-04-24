8f4a6d4ab871e42a63de4d4ac3458d95d3a96b51
Guillaume Crégut <gcregut@free.fr>
2026-04-24 13:04:37 +0200

    [Feat]: Hive: add entity

--- src/Entity/HiveKind.php
+++ src/Entity/HiveKind.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\HiveKindRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: HiveKindRepository::class)]
@@ -18,6 +20,17 @@ class HiveKind
 
     #[ORM\Column(length: 255)]
     private ?string $picture = null;
+
+    /**
+     * @var Collection<int, Hive>
+     */
+    #[ORM\OneToMany(targetEntity: Hive::class, mappedBy: 'kind')]
+    private Collection $hives;
+
+    public function __construct()
+    {
+        $this->hives = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +57,36 @@ class HiveKind
     public function setPicture(string $picture): static
     {
         $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Hive>
+     */
+    public function getHives(): Collection
+    {
+        return $this->hives;
+    }
+
+    public function addHive(Hive $hive): static
+    {
+        if (!$this->hives->contains($hive)) {
+            $this->hives->add($hive);
+            $hive->setKind($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHive(Hive $hive): static
+    {
+        if ($this->hives->removeElement($hive)) {
+            // set the owning side to null (unless already changed)
+            if ($hive->getKind() === $this) {
+                $hive->setKind(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/HiveRise.php
+++ src/Entity/HiveRise.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\HiveRiseRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: HiveRiseRepository::class)]
@@ -15,6 +17,17 @@ class HiveRise
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    /**
+     * @var Collection<int, Hive>
+     */
+    #[ORM\OneToMany(targetEntity: Hive::class, mappedBy: 'rise')]
+    private Collection $hives;
+
+    public function __construct()
+    {
+        $this->hives = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class HiveRise
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Hive>
+     */
+    public function getHives(): Collection
+    {
+        return $this->hives;
+    }
+
+    public function addHive(Hive $hive): static
+    {
+        if (!$this->hives->contains($hive)) {
+            $this->hives->add($hive);
+            $hive->setRise($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHive(Hive $hive): static
+    {
+        if ($this->hives->removeElement($hive)) {
+            // set the owning side to null (unless already changed)
+            if ($hive->getRise() === $this) {
+                $hive->setRise(null);
+            }
+        }
 
         return $this;
     }
