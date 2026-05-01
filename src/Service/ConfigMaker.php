4aa36ee8de20bdbab8d9393add0472b5d54a2ee8
Guillaume Crégut <gcregut@free.fr>
2026-05-01 18:55:14 +0200

    [Feat]: Datalogger: config maker

---
+++ src/Service/ConfigMaker.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Hive;
+use App\Dto\DataloggerConfigDto;
+use App\Entity\Apiculteur;
+use App\Exception\DataloggerFileException;
+use App\Tool\PathMaker;
+
+class ConfigMaker
+{
+    public function makeConfig(Hive $hive, DataloggerConfigDto $dto, string $path, Apiculteur $user): string
+    {
+        $fullPath = PathMaker::makeDataloggerConfigPath($user->getId(), $hive->getId(), $path);
+        if(!is_dir($fullPath)) {
+            mkdir($fullPath, 0644, true);
+        }
+        $fullPath .= 'config.json';
+        $returnpath = str_replace($path,'',$fullPath);
+        $config = (array) $dto;
+        $config['hive'] = $hive->getName();
+        $config['hiveId'] = $hive->getIdentification();
+        $template = [];
+        if($dto->extHygro) {
+            $template['extHygro'] = 'int';
+        }
+        if($dto->intHygro) {
+            $template['intHygro'] = 'int';
+        }
+        if($dto->intTemp) {
+            $template['intTemp'] = 'float';
+        }
+        if($dto->extTemp) {
+            $template['extTemp'] = 'float';
+        }
+        if($dto->weight) {
+            $template['weight'] = 'float';
+        }
+        $content = [
+            'config' =>$config,
+            'template'=>$template
+        ];
+        $encoded = json_encode($content);
+        if(!file_put_contents($fullPath,$encoded)) {
+            throw new DataloggerFileException('Config file not written');
+        }
+        return $returnpath;
+    }
+}
