740fa8e4af7796665baa66fe51affd8b965771a1
Guillaume Crégut <gcregut@free.fr>
2026-05-05 17:47:27 +0200

    [Feat]: PathMaker: add all missing users path, refactoring

--- src/Tool/PathMaker.php
+++ src/Tool/PathMaker.php
@@ -2,38 +2,133 @@
 
 namespace App\Tool;
 
+use App\Entity\Apiary;
+use App\Entity\Hive;
+use App\Entity\Apiculteur;
+
 class PathMaker
 {
-    public static function makeDataloggerConfigPath(int $userId, int $hiveId, string $basePath): string
+    /**
+     * Will create and return the absolute path where to store datalogger config file
+     * 
+     * @param Apiculteur $user 
+     * @param Hive $hive
+     * @param string $basePath : path to upload directory
+     * @return string return the whole path with the trailing DIRECTORY SEPARATOR
+     */
+    public static function makeDataloggerConfigPath(Apiculteur $user, Hive $hive, string $basePath): string
     {
-        $fullPath = $basePath . $userId . DIRECTORY_SEPARATOR;
+        $fullPath = $basePath . $user->getId() . DIRECTORY_SEPARATOR;
         $fullPath .= 'datalogger' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
-        $fullPath .= $hiveId . DIRECTORY_SEPARATOR;
-        if (!is_dir($fullPath)) {
-            mkdir($fullPath, 0766, true);
-        }
+        $fullPath .= $hive->getId() . DIRECTORY_SEPARATOR;
+        self::makeFolder($fullPath);
         return $fullPath;
     }
 
-    public static function makeDataloggerFilePath(int $userId, int $hiveId, string $basePath): string
+    /**
+     *  Will create and return the absolute path where to store datalogger data files
+     * 
+     * @param Apiculteur $user
+     * @param Hive $hive
+     * @param string $basePath : path to upload directory
+     * @return string return the whole path with the trailing DIRECTORY SEPARATOR
+     */
+    public static function makeDataloggerFilePath(Apiculteur $user, Hive $hive, string $basePath): string
     {
-        $fullPath = $basePath . $userId . DIRECTORY_SEPARATOR;
+        $fullPath = $basePath . $user->getId() . DIRECTORY_SEPARATOR;
         $fullPath .= 'datalogger' . DIRECTORY_SEPARATOR . 'datas' . DIRECTORY_SEPARATOR;
-        $fullPath .= $hiveId . DIRECTORY_SEPARATOR;
-        if (!is_dir($fullPath)) {
-            mkdir($fullPath, 0766, true);
-        }
+        $fullPath .= $hive->getId() . DIRECTORY_SEPARATOR;
+        self::makeFolder($fullPath);
         return $fullPath;
     }
 
-    public static function makeUserHiveStatPath(int $userId, int $hiveId, string $typeDoc, string $basePath): string
+    /**
+     *  Will create and return the absolute path where to store hive statistic files
+     *
+     * @param Apiculteur $user
+     * @param Hive $hive
+     * @param string $typeDoc
+     * @param string $basePath
+     * @return string return the whole path with the trailing DIRECTORY SEPARATOR
+     */
+    public static function makeUserHiveStatPath(Apiculteur $user, Hive $hive, string $typeDoc, string $basePath): string
     {
-        $fullPath = $basePath . $userId . DIRECTORY_SEPARATOR;
+        $fullPath = $basePath . $user->getId() . DIRECTORY_SEPARATOR;
         $fullPath .= 'stats' . DIRECTORY_SEPARATOR . $typeDoc . DIRECTORY_SEPARATOR;
-        $fullPath .= $hiveId . DIRECTORY_SEPARATOR;
-        if (!is_dir($fullPath)) {
+        $fullPath .= $hive->getId() . DIRECTORY_SEPARATOR;
+        self::makeFolder($fullPath);
+        return $fullPath;
+    }
+
+    /**
+     * Will create and return the relative path where to store users's apiary declaration files
+     *
+     * @param Apiculteur $user
+     * @param string $basePath
+     * @return string return the relative path with the trailing DIRECTORY SEPARATOR
+     */
+    public static function makeApiaryDeclarationPath(Apiculteur $user, string $basePath): string
+    {
+        $path = $user->getId() . DIRECTORY_SEPARATOR;
+        $path .= 'documents' . DIRECTORY_SEPARATOR;
+        $fullPath = $basePath . $path;
+        self::makeFolder($fullPath);
+        return $path;
+    }
+
+    /**
+     * Will create and return the folder where to store apiary cartography file
+     *
+     * @param Apiculteur $user
+     * @param Apiary $apiary
+     * @param string $basePath
+     * @return string return the relative path with trailing DIRECTORY SEPARATOR
+     */
+    public static function makeCartoApiaryPath(Apiculteur $user, Apiary $apiary, string $basePath): string
+    {
+        $path = $user->getId() . DIRECTORY_SEPARATOR;
+        $path .= 'carto' . DIRECTORY_SEPARATOR;
+        $fullPath = $basePath . $path;
+        self::makeFolder($fullPath);
+        return $path;
+    }
+
+    /**
+     * Will make admin folder to upload honey pictures and return relative path
+     *
+     * @param string $basePath
+     * @return string : relative path to honey folder with trailing DIRECTORY_SEPARATOR
+     */
+    public static function makeHoneyFolder(string $basePath): string
+    {
+        $path = 'admin' . DIRECTORY_SEPARATOR;
+        $path .= 'honeys' . DIRECTORY_SEPARATOR;
+        $fullPath = $basePath . $path;
+        self::makeFolder($fullPath);
+        return $path;
+    }
+
+    /**
+     * Will make admin folder to upload hives pictures and return relative path
+     *
+     * @param string $basePath
+     * @return string : relative path to hives folder with trailing DIRECTORY_SEPARATOR
+     */
+    public static function makeAdminHivesFolder(string $basePath): string
+    {
+        $path = 'admin' . DIRECTORY_SEPARATOR;
+        $path .= 'hives' . DIRECTORY_SEPARATOR;
+        $fullPath = $basePath . $path;
+        self::makeFolder($fullPath);
+        return $path;
+    }
+
+
+
+    private static function makeFolder(string $fullPath): void
+    {
+         if (!is_dir($fullPath)) {
             mkdir($fullPath, 0766, true);
         }
-        return $fullPath;
     }
 }
