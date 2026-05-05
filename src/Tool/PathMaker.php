6e10e1f6b99ecc152004d1021a6d5052047f60d8
Guillaume Crégut <gcregut@free.fr>
2026-05-05 11:44:27 +0200

    [Feat]: PathMaker add builind dir for config path

--- src/Tool/PathMaker.php
+++ src/Tool/PathMaker.php
@@ -6,10 +6,13 @@ class PathMaker
 {
     public static function makeDataloggerConfigPath(int $userId, int $hiveId, string $basePath): string
     {
-         $fullPath = $basePath . $userId . DIRECTORY_SEPARATOR;
-         $fullPath .= 'datalogger' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
-         $fullPath .= $hiveId . DIRECTORY_SEPARATOR;
-         return $fullPath;
+        $fullPath = $basePath . $userId . DIRECTORY_SEPARATOR;
+        $fullPath .= 'datalogger' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
+        $fullPath .= $hiveId . DIRECTORY_SEPARATOR;
+        if (!is_dir($fullPath)) {
+            mkdir($fullPath, 0766, true);
+        }
+        return $fullPath;
     }
 
     public static function makeDataloggerFilePath(int $userId, int $hiveId, string $basePath): string
