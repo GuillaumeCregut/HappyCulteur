6942bb7d2d5a7b3304ada9593324a20059ea47c2
Guillaume Crégut <gcregut@free.fr>
2026-05-05 11:51:04 +0200

    [Fix]: Visits service: fi dateTime error

--- src/Service/Stats/Visits.php
+++ src/Service/Stats/Visits.php
@@ -3,25 +3,43 @@
 namespace App\Service\Stats;
 
 use App\Entity\Hive;
-use App\Repository\VisitRepository;
+use App\Entity\Visit;
 use DateTimeImmutable;
+use App\Tool\EditielPdf;
+use App\Repository\VisitRepository;
+use App\Tool\PathMaker;
+use Exception;
 
 class Visits
 {
-    public function __construct(private VisitRepository $repo){}
+    public function __construct(private VisitRepository $repo, private EditielPdf $pdf) {}
 
-    public function getHiveVisits(Hive $hive, array $dates): array
+    public function getHiveVisits(Hive $hive, array $dates, string $basePath): array
     {
-        $result =[];
-        /**@var DateTimeImmutable $startDate, $endDate */
+        $result = [];
+        /**@var \DateTime $startDate, $endDate */
         $startDate = $dates['startDate'];
         $endDate = $dates['endDate'];
-        if(null === $endDate) {
+        if (null === $endDate) {
             $endDate = new DateTimeImmutable('now');
+        } else {
+            $endDate = DateTimeImmutable::createFromMutable($endDate);
+        }
+        if(null !== $startDate) {   
+            $startDate = DateTimeImmutable::createFromMutable($startDate);
         }
         $result['startDate'] = $startDate === null ? 'début' : $startDate->format('d/m/Y');
         $result['endDate'] = $endDate->format('d/m/Y');
+        $visits =  $this->repo->findByhiveAndDates($hive, $endDate, $startDate);
         $result['visits'] =  $this->repo->findByhiveAndDates($hive, $endDate, $startDate);
+        if (0 < count($visits)) {
+            if (null === $startDate) {
+                $startDate = new DateTimeImmutable('1970-01-01');
+            }
+            $result['path'] = $this->buildPdf($visits, $hive, $startDate, $endDate, $basePath);
+        } else {
+            $result['path'] = null;
+        }
         return $result;
     }
 }
