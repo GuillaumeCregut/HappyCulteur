cdc71668b83794adee4285209083b4043d17edaa
Guillaume Crégut <gcregut@free.fr>
2026-05-05 11:58:38 +0200

    [Fix]: StatsController: fix error when session is empty

--- src/Controller/StatsController.php
+++ src/Controller/StatsController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Hive;
 use App\Form\Stats\DatesType;
 use App\Service\Stats\Visits;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/stats', name: 'app_stats_')]
@@ -16,8 +18,7 @@ final class StatsController extends AbstractController
     #[Route('/hive/{id}', name: 'index')]
     public function index(
         Hive $hive
-    ): Response
-    {
+    ): Response {
         $this->denyAccessUnlessGranted('own', $hive);
         return $this->render('stats/index.html.twig', [
             'hive' => $hive,
@@ -26,35 +27,46 @@ final class StatsController extends AbstractController
 
     #[Route('/hive/{id}/visits', name: 'visits_search', methods: ['GET', 'POST'])]
     public function visits(
-        Hive $hive, 
+        Hive $hive,
         Request $request,
-        Visits $visits, 
+        Visits $visits,
+        #[Autowire('%kernel.project_dir%/public/uploads/')] string $uploadDirectory
     ): Response {
         $this->denyAccessUnlessGranted('own', $hive);
-        $form = $this->createForm(DatesType::class,null);
+        $form = $this->createForm(DatesType::class, null);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $dates = $form->getData();
-            $visits = $visits->getHiveVisits($hive,$dates);
+            $visits = $visits->getHiveVisits($hive, $dates, $uploadDirectory);
             $request->getSession()->set('visit_results', $visits);
             return $this->redirectToRoute('app_stats_visits_results', ['id' => $hive->getId()]);
         }
         return $this->render('stats/visits/search.html.twig', [
-            'id' => $hive->getId(), 
+            'id' => $hive->getId(),
             'form' => $form,
             'hive' => $hive
-            ]);
+        ]);
     }
 
     #[Route('/hive/{id}/visits/results', name: 'visits_results', methods: ['GET'])]
     public function displayResultVisit(Hive $hive, Request $request): Response
     {
         $visits = $request->getSession()->get('visit_results');
-        return $this->render('stats/visits/details.html.twig', [
-                'hive' => $hive,
-                'visits' => $visits['visits'],
-                'endDate' => $visits['endDate'],
-                'startDate' => $visits['startDate'] 
+        if (null === $visits) {
+            $form = $this->createForm(DatesType::class, null);
+            $form->addError(new FormError("Une erreur est survenue, veuillez recommencer"));
+            return $this->render('stats/visits/search.html.twig', [
+                'id' => $hive->getId(),
+                'form' => $form,
+                'hive' => $hive
             ]);
+        }
+        return $this->render('stats/visits/details.html.twig', [
+            'hive' => $hive,
+            'visits' => $visits['visits'],
+            'endDate' => $visits['endDate'],
+            'startDate' => $visits['startDate'],
+            'doc' => $visits['path']
+        ]);
     }
 }
