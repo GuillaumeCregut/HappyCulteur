55a666a38fc49f0ae0b7b3c17f47b45fc61c57fe
Guillaume Crégut <gcregut@free.fr>
2026-05-11 14:01:27 +0200

    [Feat]: LineGraph: add multiline and marks on plots

--- src/Tool/LineGraph.php
+++ src/Tool/LineGraph.php
@@ -34,36 +34,62 @@ class LineGraph
         $this->maxXValue = $maxValue;
     }
 
+    public function setMinMax(int $min, int $max): void
+    {
+        $this->minXValue = $min;
+        $this->maxXValue = $max;
+    }
+
     /**
      * Add a new line to graph
      * xAxisValues must be set before adding a new line
-     *
+     * Must be used with SetMinMax function (to setup the x scale value)
      * @param array $values
+     * @param string|null $legend
+     * @param string|null $color
+     * @param boolean|null $isMarked
+     * @param string|null $picture
+     * @param string|null $markColor
+     * 
      * @return boolean true if line is added, false else
      */
-    public function addLine(array $values, ?string $legend= null, ?string $color=null): bool
+    public function addLine(array $values, ?string $legend = null, ?string $color = null, ?bool $isMarked = false, ?string $picture = null, ?string $markColor = null): bool
     {
         if (0 === ($this->xValues)) {
             return false;
         }
-        $plot = new LinePlot($values, $this->xValues);
-        if(null !== $legend) {
-            $plot->SetLegend($legend);
-        }
-        if(null !== $color) {
-            $plot->setColor($color);
-        }
-        $this->lines[] = $plot;
+        $this->addPlot($values, $this->xValues, $legend, $color, $isMarked, $picture, $markColor);
         return true;
+    }
+
+    /**
+     * Create a line that have its own x values.
+     *
+     * @param array $values
+     * @param array|null $xValues
+     * @param string|null $legend
+     * @param string|null $color
+     * @param boolean|null $isMarked
+     * @param string|null $picture
+     * @param string|null $markColor
+     * @return void
+     */
+    public function addMultiValuesLine(array $values, ?array $xValues = null,  ?string $legend = null, ?string $color = null, ?bool $isMarked = false, ?string $picture = null, ?string $markColor = null)
+    {
+        if (null === $xValues) {
+            $xValues = $this->xValues;
+        }
+        $this->addPlot($values, $xValues, $legend, $color, $isMarked, $picture, $markColor);
     }
 
     /**
      * Draw Graph in memory
      * X axis and 1 line must be set before
      * @param string|null $title
+     * @param int|null $legendWidth : How many time width will be divide to make legend width (default 2)
      * @return boolean return true if graph is create, false else
      */
-    public function drawGraph(?string $title = null): bool
+    public function drawGraph(?string $title = null, ?int $legendWidth = 2): bool
     {
         if (0 >= count($this->lines)) {
             return false;
@@ -85,7 +111,7 @@ class LineGraph
         foreach ($this->lines as $line) {
             $this->graph->add($line);
         }
-        $xLegend = $this->width - ($this->width / 2);
+        $xLegend = $this->width - ($this->width / $legendWidth);
         $yLegend = 40;
         $this->graph->legend->SetAbsPos($xLegend, $yLegend, 'center', 'bottom');
         return true;
@@ -100,5 +126,36 @@ class LineGraph
     public function formDate(mixed $aVal)
     {
         return date('d-m-y', $aVal);
+    }
+
+    private function createRandomColor(): array
+    {
+        $Radomizer = rand(1, 15);
+        $R = rand(0, 4096) / (rand(1, 16) + $Radomizer);
+        $V = rand(0, 2048) / rand(1, 8);
+        $B = rand(0, 8192) / (rand(1, 32));
+        return  array($R, $V, $B);
+    }
+
+    private function addPlot(array $yDatas, array $xDatas, ?string $legend = null, ?string $color = null, ?bool $isMarked = false, ?string $picture = null, ?string $markColor = null)
+    {
+        $plot = new LinePlot($yDatas, $xDatas);
+        if (null !== $legend) {
+            $plot->SetLegend($legend);
+        }
+        if (null !== $color) {
+            $plot->setColor($color);
+        }
+        if ($isMarked) {
+            $plot->mark->SetColor($markColor);
+            $markFillColor = $this->createRandomColor();
+            $plot->mark->SetFillColor($markFillColor);
+            if (null !== $picture) {
+                $plot->mark->SetType(MARK_IMG, $picture, '1');
+            } else {
+                $plot->mark->SetType(MARK_DIAMOND);
+            }
+        }
+        $this->lines[] = $plot;
     }
 }
