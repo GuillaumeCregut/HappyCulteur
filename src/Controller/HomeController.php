c1402d5ba07b6aabaee43e2e572f08834dd866b8
Guillaume Crégut <gcregut@free.fr>
2026-05-21 18:03:10 +0200

    [Feat] Params: add system info and appearence

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -71,4 +71,11 @@ final class HomeController extends AbstractController
         $version = $this->getParameter('app.datalogger.version');
         return $this->render('params/infos_system.html.twig', ['version' => $version]);
     }
+
+    #[Route(path: '/params/appearence', name: 'params_appearence')]
+    #[IsGranted('ROLE_USER')]
+    public function appearence(): Response
+    {
+        return $this->render('params/appearence.html.twig', []);
+    }
 }
